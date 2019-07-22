691ef3c23a1716439955cb27f5c604b35a0b0911
Ryan <631811480@qq.com>
2019-07-22 17:48:32 +0800

    springboot构建微服务实战

--- mallweb/src/main/java/com/ypy/mallweb/App.java
+++ mallweb/src/main/java/com/ypy/mallweb/App.java
@@ -1,8 +1,19 @@
 package com.ypy.mallweb;
 
+import com.google.gson.Gson;
+import com.ypy.mallweb.bean.Response;
+import org.springframework.web.client.RestTemplate;
+
 public class App {
 
+    private static String BASE_URL = "http://127.0.0.1:8080";
+
     public static void main(String[] args) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String body = restTemplate.getForObject(BASE_URL + "/get/1" ,String.class);
+        Response response = new Gson().fromJson(body, Response.class);
+        System.out.println(response);
 
     }
 
