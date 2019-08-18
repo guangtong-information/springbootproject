75beafc217f37bf7819681b9e1d87da27a96f4a2
Ryan <631811480@qq.com>
2019-08-18 15:17:03 +0800

    服务的注册和发现

--- mallproduct/db/db.sql
+++ mallproduct/db/db.sql
@@ -3,3 +3,4 @@ create database demo_test1 default charset utf-8;
 
 -- 产品表
 create table products(pid int not null primary key auto_increment,pname varchar (200),type varchar (50),price double ,createtime timestamp )
+
